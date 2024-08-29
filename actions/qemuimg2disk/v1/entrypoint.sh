e0b8494c7179a2b1adf74cf761423aca652d60ee
Amit Kumar <amit2.kumar@intel.com>
2024-08-29 12:22:21 +0530

    Update entrypoint.sh

--- actions/qemuimg2disk/v1/entrypoint.sh
+++ actions/qemuimg2disk/v1/entrypoint.sh
@@ -3,5 +3,6 @@ set -o errexit
 set -o xtrace
 # We actually do want EXTRA_ARGS to be split up
 #shellcheck disable=SC2086
-qemu-img convert "${IMG_URL:?}" -O "${FORMAT:-host_device}" "${DEST_DISK:?}" ${EXTRA_ARGS}
+curl "${IMG_URL:?}" -O jammy-server-cloudimg-amd64.img
+qemu-img convert jammy-server-cloudimg-amd64.img -O "${FORMAT:-host_device}" "${DEST_DISK:?}" ${EXTRA_ARGS}
 partprobe || true
